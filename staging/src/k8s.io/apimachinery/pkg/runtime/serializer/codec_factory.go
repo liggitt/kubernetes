acc474a4d4b75f1e936bc863d71cd4ee49c3124d
Kevin Delgado <kevindelgado@google.com>
2021-10-13 21:48:28 +0000

    bisect 2

--- staging/src/k8s.io/apimachinery/pkg/runtime/serializer/codec_factory.go
+++ staging/src/k8s.io/apimachinery/pkg/runtime/serializer/codec_factory.go
@@ -40,7 +40,7 @@ type serializerType struct {
 
 	Serializer       runtime.Serializer
 	PrettySerializer runtime.Serializer
-	StrictSerializer runtime.Serializer
+	//StrictSerializer runtime.Serializer
 
 	AcceptStreamContentTypes []string
 	StreamContentType        string
@@ -70,19 +70,19 @@ func newSerializersForScheme(scheme *runtime.Scheme, mf json.MetaFactory, option
 			json.SerializerOptions{Yaml: false, Pretty: true, Strict: options.Strict},
 		)
 	}
-	strictJSONSerializer := json.NewSerializerWithOptions(
-		mf, scheme, scheme,
-		json.SerializerOptions{Yaml: false, Pretty: false, Strict: true},
-	)
-	jsonSerializerType.StrictSerializer = strictJSONSerializer
+	//strictJSONSerializer := json.NewSerializerWithOptions(
+	//	mf, scheme, scheme,
+	//	json.SerializerOptions{Yaml: false, Pretty: false, Strict: true},
+	//)
+	//jsonSerializerType.StrictSerializer = strictJSONSerializer
 	yamlSerializer := json.NewSerializerWithOptions(
 		mf, scheme, scheme,
 		json.SerializerOptions{Yaml: true, Pretty: false, Strict: options.Strict},
 	)
-	strictYAMLSerializer := json.NewSerializerWithOptions(
-		mf, scheme, scheme,
-		json.SerializerOptions{Yaml: true, Pretty: false, Strict: true},
-	)
+	//strictYAMLSerializer := json.NewSerializerWithOptions(
+	//	mf, scheme, scheme,
+	//	json.SerializerOptions{Yaml: true, Pretty: false, Strict: true},
+	//)
 	protoSerializer := protobuf.NewSerializer(scheme, scheme)
 	protoRawSerializer := protobuf.NewRawSerializer(scheme, scheme)
 
@@ -94,7 +94,7 @@ func newSerializersForScheme(scheme *runtime.Scheme, mf json.MetaFactory, option
 			FileExtensions:     []string{"yaml"},
 			EncodesAsText:      true,
 			Serializer:         yamlSerializer,
-			StrictSerializer:   strictYAMLSerializer,
+			//StrictSerializer:   strictYAMLSerializer,
 		},
 		{
 			AcceptContentTypes: []string{runtime.ContentTypeProtobuf},
@@ -199,7 +199,7 @@ func newCodecFactory(scheme *runtime.Scheme, serializers []serializerType) Codec
 				EncodesAsText:    d.EncodesAsText,
 				Serializer:       d.Serializer,
 				PrettySerializer: d.PrettySerializer,
-				StrictSerializer: d.StrictSerializer,
+				//StrictSerializer: d.StrictSerializer,
 			}
 
 			mediaType, _, err := mime.ParseMediaType(info.MediaType)
