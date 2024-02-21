7e9708f827cca97dc26626829c9f67ff7f56dceb
Sean Sullivan <seans@google.com>
2024-02-21 08:56:07 +0000

    portforward: tunnel spdy through websockets

---
+++ staging/src/k8s.io/apiserver/pkg/util/proxy/streamtunnel.go
@@ -0,0 +1,115 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"bufio"
+	"net"
+	"net/http"
+
+	gwebsocket "github.com/gorilla/websocket"
+
+	"k8s.io/apimachinery/pkg/util/httpstream"
+	"k8s.io/apimachinery/pkg/util/httpstream/spdy"
+	"k8s.io/apimachinery/pkg/util/httpstream/wsstream"
+	utilnet "k8s.io/apimachinery/pkg/util/net"
+	constants "k8s.io/apimachinery/pkg/util/portforward"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/klog/v2"
+)
+
+// TunnelingHandler is a handler which tunnels SPDY through WebSockets.
+type TunnelingHandler struct {
+	// UpgradeAwareHandler used to communicate to upstream SPDY.
+	upgradeHandler http.Handler
+}
+
+func NewTunnelingHandler(upgradeHandler http.Handler) *TunnelingHandler {
+	return &TunnelingHandler{upgradeHandler: upgradeHandler}
+}
+
+func (h *TunnelingHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	klog.Infoln("TunnelingHandler ServeHTTP")
+	clone := utilnet.CloneRequest(req)
+
+	// First, terminate the websocket connection, creating the net.Conn
+	// that will be used to tunnel SPDY.
+	klog.Infoln("Upgrading, terminating websocket connection...")
+	var upgrader = gwebsocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true // Accepting all requests
+		},
+		Subprotocols: []string{
+			constants.PortForwardV2Name,
+		},
+	}
+	conn, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		klog.Errorf("error upgrading websocket connection: %v", err)
+		return
+	}
+	defer conn.Close() //nolint:errcheck
+	klog.Infof("websocket connection created: %s", conn.Subprotocol())
+	tunnelingConn := portforward.NewTunnelingConnection(conn)
+
+	// Create ResponseWriter which will be hijacked to use the tunnel.
+	writer := createTunnelingResponseWriter(w, tunnelingConn)
+
+	// Remove all the websocket headers
+	clone.Header.Del(wsstream.WebSocketProtocolHeader)
+	clone.Header.Del("Sec-Websocket-Key")
+	clone.Header.Del("Sec-Websocket-Version")
+	clone.Header.Del(httpstream.HeaderUpgrade)
+	// Add all the SPDY headers
+	clone.Header.Add(httpstream.HeaderUpgrade, spdy.HeaderSpdy31)
+	clone.Header.Add(httpstream.HeaderProtocolVersion, "portforward.k8s.io")
+
+	// UpgradeHandler will first create the upstream SPDY connection,
+	// copying SPDY data between the upstream connection and
+	// the tunneling websocket connection (hijacked from writer).
+	klog.Infoln("Calling upgrade aware proxy...")
+	h.upgradeHandler.ServeHTTP(writer, clone)
+}
+
+var _ http.ResponseWriter = &tunnelingWriter{}
+var _ http.Hijacker = &tunnelingWriter{}
+
+// tunnelingWriter implements the http.ResponseWriter and http.Hijcker
+// interfaces. After "Hijack", the stored ResponseWriter will not work.
+type tunnelingWriter struct {
+	conn net.Conn
+	w    http.ResponseWriter
+}
+
+func createTunnelingResponseWriter(w http.ResponseWriter, tunnel net.Conn) http.ResponseWriter {
+	return &tunnelingWriter{conn: tunnel, w: w}
+}
+
+func (w *tunnelingWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	klog.Infof("hijacked--returning websocket tunneling net.Conn")
+	return w.conn, nil, nil
+}
+
+func (w *tunnelingWriter) Header() http.Header {
+	return http.Header{}
+}
+
+func (w *tunnelingWriter) Write(p []byte) (int, error) {
+	return w.w.Write(p)
+}
+
+func (w *tunnelingWriter) WriteHeader(statusCode int) {}
---
+++ staging/src/k8s.io/client-go/tools/portforward/tunneling_dialer.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package portforward
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"k8s.io/apimachinery/pkg/util/httpstream"
+	"k8s.io/apimachinery/pkg/util/httpstream/spdy"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/transport/websocket"
+	"k8s.io/klog/v2"
+)
+
+// tunnelingDialer implements "httpstream.Dial" interface
+type tunnelingDialer struct {
+	url       *url.URL
+	transport http.RoundTripper
+	holder    websocket.ConnectionHolder
+}
+
+// NewTunnelingDialer creates and returns the tunnelingDialer structure which implemements the "httpstream.Dialer"
+// interface. The dialer can upgrade a websocket request, creating a websocket connection. This function
+// returns an error if one occurs.
+func NewTunnelingDialer(url *url.URL, config *restclient.Config) (httpstream.Dialer, error) {
+	transport, holder, err := websocket.RoundTripperFor(config)
+	if err != nil {
+		return nil, err
+	}
+	return &tunnelingDialer{
+		url:       url,
+		transport: transport,
+		holder:    holder,
+	}, nil
+}
+
+// Dial upgrades a websocket request, returning a websocket connection (wrapped
+// by an "httpstream.Connection"), the negotiated protocol, or an error if one occurred.
+func (d *tunnelingDialer) Dial(protocols ...string) (httpstream.Connection, string, error) {
+	// There is no passed context, so skip the context when creating request for now.
+	// Websockets requires "GET" method: RFC 6455 Sec. 4.1 (page 17).
+	req, err := http.NewRequest("GET", d.url.String(), nil)
+	if err != nil {
+		return nil, "", err
+	}
+	// Hard-code the v2 portforward protocol for the websocket dialer for now.
+	websocketProtocols := []string{"v2.portforward.k8s.io"}
+	klog.V(4).Infoln("Before WebSocket Upgrade Connection...")
+	conn, err := websocket.Negotiate(d.transport, d.holder, req, websocketProtocols...)
+	if err != nil {
+		return nil, "", err
+	}
+	if conn == nil {
+		return nil, "", fmt.Errorf("negotiated websocket connection is nil")
+	}
+	protocol := conn.Subprotocol()
+	klog.V(4).Infof("negotiated protocol: %s", protocol)
+
+	// Create tunneling websocket connection implementing net.Conn
+	tConn := NewTunnelingConnection(conn)
+	// Create SPDY connection with the previously created tConn.
+	spdyConn, err := spdy.NewClientConnectionWithPings(tConn, 0)
+
+	return spdyConn, protocol, err
+}
