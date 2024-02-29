265b527bbd1dfbfb11b84fc5f170a8f17a638ce5
Jordan Liggitt <liggitt@google.com>
2024-02-29 08:28:48 -0500

    Make tunnelingResponseWriter threadsafe, make Write/Hijack mutually exclusive

--- staging/src/k8s.io/apiserver/pkg/util/proxy/streamtunnel.go
+++ staging/src/k8s.io/apiserver/pkg/util/proxy/streamtunnel.go
@@ -24,6 +24,7 @@ import (
 	"net"
 	"net/http"
 	"strings"
+	"sync"
 
 	gwebsocket "github.com/gorilla/websocket"
 
@@ -80,9 +81,14 @@ func (h *TunnelingHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	tunnelProtocol := conn.Subprotocol()
 	klog.V(4).Infof("websocket connection created: %s", tunnelProtocol)
 	tunnelingConn := portforward.NewTunnelingConnection("server", conn)
-	headerInterceptingConnection := &headerInterceptingConnection{Conn: tunnelingConn, headerBuffer: bytes.NewBuffer(nil)}
-	// Create ResponseWriter which will be hijacked to use the tunnel.
-	writer := createTunnelingResponseWriter(w, headerInterceptingConnection)
+
+	writer := &tunnelingResponseWriter{
+		w: w,
+		conn: &headerInterceptingConnection{
+			Conn:         tunnelingConn,
+			headerBuffer: bytes.NewBuffer(nil),
+		},
+	}
 
 	klog.V(4).Infoln("Tunnel spdy through websockets using the UpgradeAwareProxy")
 	h.upgradeHandler.ServeHTTP(writer, spdyRequest)
@@ -130,39 +136,84 @@ func websocketProtocolsFromSPDYProtocols(spdyProtocols []string) []string {
 	return websocketProtocols
 }
 
-var _ http.ResponseWriter = &tunnelingWriter{}
-var _ http.Hijacker = &tunnelingWriter{}
+var _ http.ResponseWriter = &tunnelingResponseWriter{}
+var _ http.Hijacker = &tunnelingResponseWriter{}
 
-// tunnelingWriter implements the http.ResponseWriter and http.Hijcker
-// interfaces. After "Hijack", the stored ResponseWriter will not work.
-type tunnelingWriter struct {
+// tunnelingResponseWriter implements the http.ResponseWriter and http.Hijacker interfaces.
+// Only non-upgrade responses can be written using WriteHeader() and Write().
+// Once Write or WriteHeader is called, Hijack returns an error.
+// Once Hijack is called, Write, WriteHeader, and Hijack return errors.
+type tunnelingResponseWriter struct {
+	// w is used to delegate Header(), WriteHeader(), and Write() calls
+	w http.ResponseWriter
+	// conn is returned from Hijack()
 	conn net.Conn
-	w    http.ResponseWriter
+	// mu guards writes
+	mu sync.Mutex
+	// wrote tracks whether WriteHeader or Write has been called
+	written bool
+	// hijacked tracks whether Hijack has been called
+	hijacked bool
 }
 
-func createTunnelingResponseWriter(w http.ResponseWriter, tunnel net.Conn) http.ResponseWriter {
-	return &tunnelingWriter{conn: tunnel, w: w}
-}
-
-// Hijack returns a "net.Conn" which tunnels SPDY through WebSockets.
-// The returned bufio.ReadWriter and error are always nil.
-func (w *tunnelingWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+// Hijack returns a delegate "net.Conn".
+// An error is returned if Write(), WriteHeader(), or Hijack() was previously called.
+// The returned bufio.ReadWriter is always nil.
+func (w *tunnelingResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.written {
+		klog.Errorf("Hijack called after write")
+		return nil, nil, errors.New("connection has already been written to")
+	}
+	if w.hijacked {
+		klog.Errorf("Hijack called after hijack")
+		return nil, nil, errors.New("connection has already been hijacked")
+	}
+	w.hijacked = true
 	klog.V(6).Infof("Hijack returning websocket tunneling net.Conn")
 	return w.conn, nil, nil
 }
 
 // Header is delegated to the stored "http.ResponseWriter".
-func (w *tunnelingWriter) Header() http.Header {
+func (w *tunnelingResponseWriter) Header() http.Header {
 	return w.w.Header()
 }
 
 // Write is delegated to the stored "http.ResponseWriter".
-func (w *tunnelingWriter) Write(p []byte) (int, error) {
+func (w *tunnelingResponseWriter) Write(p []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.hijacked {
+		klog.Errorf("Write called after hijack")
+		return 0, http.ErrHijacked
+	}
+	w.written = true
 	return w.w.Write(p)
 }
 
 // WriteHeader is delegated to the stored "http.ResponseWriter".
-func (w *tunnelingWriter) WriteHeader(statusCode int) {
+func (w *tunnelingResponseWriter) WriteHeader(statusCode int) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.written {
+		klog.Errorf("WriteHeader called after write")
+		return
+	}
+	if w.hijacked {
+		klog.Errorf("WriteHeader called after hijack")
+		return
+	}
+	w.written = true
+
+	if statusCode == http.StatusSwitchingProtocols {
+		// 101 upgrade responses must come via the hijacked connection, not WriteHeader
+		klog.Errorf("WriteHeader called with 101 upgrade")
+		http.Error(w.w, "unexpected upgrade", http.StatusInternalServerError)
+		return
+	}
+
+	// pass through non-upgrade responses we don't need to translate
 	w.w.WriteHeader(statusCode)
 }
 
