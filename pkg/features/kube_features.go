d62e726115907a3c28e5cb1ff7e0df31cb3e99c7
David Eads <deads@redhat.com>
2024-05-23 15:12:26 -0400

    add field and label selectors to authorization attributes

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1230,6 +1230,8 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	genericfeatures.APIServingWithRoutine: {Default: true, PreRelease: featuregate.Beta},
 
+	genericfeatures.AuthorizeWithSelectors: {Default: false, PreRelease: featuregate.Alpha},
+
 	genericfeatures.ConsistentListFromCache: {Default: true, PreRelease: featuregate.Beta},
 
 	genericfeatures.CustomResourceValidationExpressions: {Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.31
