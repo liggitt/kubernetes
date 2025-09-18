kubernetes/pod-security-admission

drop UserNamespacesPodSecurityStandards feature gate
this feature gate was meant to be ephemeral, and only was used for guaranteeing a
cluster admin didn't accidentally relax PSA policies before the kubelet would deny a pod
was created if it didn't support user namespaces. As of kube 1.33, the supported apiserver version
skew of n-3 guarantees that all supported kubelets are of 1.30 or later, meaning they do this.

Now, we can unconditionally relax PSA policy if a pod is in a user namespace.

This PR reserves older policies default behavior by never relaxing

Signed-off-by: Peter Hunt <pehunt@redhat.com>

Kubernetes-commit: e8bd3f629d435797c586726d3b76fd90d7b5bf08

policy/check_procMount_baseline.go
@@ -27,27 +27,25 @@ import (
 
 /*
 
-The default /proc masks are set up to reduce attack surface, and should be required.
+The default /proc masks are set up to reduce attack surface, and should be required
+by the baseline policy unless the pod is in a user namespace ("hostUsers: false").
 
 **Restricted Fields:**
 spec.containers[*].securityContext.procMount
 spec.initContainers[*].securityContext.procMount
 
-**Allowed Values:** undefined/null, "Default"
-
-However, if the pod is in a user namespace (`hostUsers: false`), and the
-UserNamespacesPodSecurityStandards feature is enabled, all values are allowed.
-
+**Allowed Values:** undefined/null, "Default" (or any value if "hostUsers" is false)
 */
 
 func init() {
-	addCheck(CheckProcMount)
+	addCheck(CheckProcMountBaseline)
 }
 
 // CheckProcMount returns a baseline level check that restricts
 // setting the value of securityContext.procMount to DefaultProcMount
-// in 1.0+
-func CheckProcMount() Check {
+// in 1.0+.
+// Starting in 1.35+, any value is allowed if the pod is in a user namespace ("hostUsers: false").
+func CheckProcMountBaseline() Check {
 	return Check{
 		ID:    "procMount",
 		Level: api.LevelBaseline,
@@ -56,19 +54,16 @@ func CheckProcMount() Check {
 				MinimumVersion: api.MajorMinorVersion(1, 0),
 				CheckPod:       procMount_1_0,
 			},
+			{
+				MinimumVersion: api.MajorMinorVersion(1, 35),
+				CheckPod:       procMount1_35baseline,
+			},
 		},
 	}
 }
 
+// procMount_1_0 blocks unmasked procMount unconditionally
 func procMount_1_0(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) CheckResult {
-	// TODO: When we remove the UserNamespacesPodSecurityStandards feature gate (and GA this relaxation),
-	// create a new policy version.
-	// Note: pod validation will check for well formed procMount type, so avoid double validation and allow everything
-	// here.
-	if relaxPolicyForUserNamespacePod(podSpec) {
-		return CheckResult{Allowed: true}
-	}
-
 	var badContainers []string
 	forbiddenProcMountTypes := sets.NewString()
 	visitContainers(podSpec, func(container *corev1.Container) {
@@ -99,4 +94,13 @@ func procMount_1_0(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) Chec
 		}
 	}
 	return CheckResult{Allowed: true}
+}
+
+// procMount1_35baseline blocks unmasked procMount for pods that are not in a user namespace
+func procMount1_35baseline(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) CheckResult {
+	if relaxPolicyForUserNamespacePod(podSpec) {
+		return CheckResult{Allowed: true}
+	}
+	// If the pod is not in a user namespace, treat it as restricted.
+	return procMount_1_0(podMetadata, podSpec)
 }

policy/check_procMount_baseline_test.go
@@ -20,21 +20,20 @@ import (
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
 )
 
-func TestProcMount(t *testing.T) {
+func TestProcMountBaseline(t *testing.T) {
 	defaultValue := corev1.DefaultProcMount
 	unmaskedValue := corev1.UnmaskedProcMount
 	otherValue := corev1.ProcMountType("other")
 
-	hostUsers := false
 	tests := []struct {
-		name           string
-		pod            *corev1.Pod
-		expectReason   string
-		expectDetail   string
-		expectAllowed  bool
-		relaxForUserNS bool
+		name          string
+		pod           *corev1.Pod
+		expectReason  string
+		expectDetail  string
+		expectAllowed bool
 	}{
 		{
 			name: "procMount",
@@ -46,14 +45,14 @@ func TestProcMount(t *testing.T) {
 					{Name: "d", SecurityContext: &corev1.SecurityContext{ProcMount: &unmaskedValue}},
 					{Name: "e", SecurityContext: &corev1.SecurityContext{ProcMount: &otherValue}},
 				},
-				HostUsers: &hostUsers,
+				HostUsers: ptr.To(true),
 			}},
 			expectReason:  `procMount`,
 			expectAllowed: false,
 			expectDetail:  `containers "d", "e" must not set securityContext.procMount to "Unmasked", "other"`,
 		},
 		{
-			name: "procMount",
+			name: "procMount with userns",
 			pod: &corev1.Pod{Spec: corev1.PodSpec{
 				Containers: []corev1.Container{
 					{Name: "a", SecurityContext: nil},
@@ -62,24 +61,17 @@ func TestProcMount(t *testing.T) {
 					{Name: "d", SecurityContext: &corev1.SecurityContext{ProcMount: &unmaskedValue}},
 					{Name: "e", SecurityContext: &corev1.SecurityContext{ProcMount: &otherValue}},
 				},
-				HostUsers: &hostUsers,
+				HostUsers: ptr.To(false),
 			}},
-			expectReason:   "",
-			expectDetail:   "",
-			expectAllowed:  true,
-			relaxForUserNS: true,
+			expectReason:  "",
+			expectDetail:  "",
+			expectAllowed: true,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			if tc.relaxForUserNS {
-				RelaxPolicyForUserNamespacePods(true)
-				t.Cleanup(func() {
-					RelaxPolicyForUserNamespacePods(false)
-				})
-			}
-			result := procMount_1_0(&tc.pod.ObjectMeta, &tc.pod.Spec)
+			result := procMount1_35baseline(&tc.pod.ObjectMeta, &tc.pod.Spec)
 			if result.Allowed != tc.expectAllowed {
 				t.Fatalf("expected Allowed to be %v was %v", tc.expectAllowed, result.Allowed)
 			}

liggitt/kubernetes

staging/src/k8s.io/pod-security-admission/policy/check_procMount_baseline.go
@@ -27,27 +27,25 @@ import (
 
 /*
 
-The default /proc masks are set up to reduce attack surface, and should be required.
+The default /proc masks are set up to reduce attack surface, and should be required
+by the baseline policy unless the pod is in a user namespace ("hostUsers: false").
 
 **Restricted Fields:**
 spec.containers[*].securityContext.procMount
 spec.initContainers[*].securityContext.procMount
 
-**Allowed Values:** undefined/null, "Default"
-
-However, if the pod is in a user namespace (`hostUsers: false`), and the
-UserNamespacesPodSecurityStandards feature is enabled, all values are allowed.
-
+**Allowed Values:** undefined/null, "Default" (or any value if "hostUsers" is false)
 */
 
 func init() {
-	addCheck(CheckProcMount)
+	addCheck(CheckProcMountBaseline)
 }
 
 // CheckProcMount returns a baseline level check that restricts
 // setting the value of securityContext.procMount to DefaultProcMount
-// in 1.0+
-func CheckProcMount() Check {
+// in 1.0+.
+// Starting in 1.35+, any value is allowed if the pod is in a user namespace ("hostUsers: false").
+func CheckProcMountBaseline() Check {
 	return Check{
 		ID:    "procMount",
 		Level: api.LevelBaseline,
@@ -56,19 +54,16 @@ func CheckProcMount() Check {
 				MinimumVersion: api.MajorMinorVersion(1, 0),
 				CheckPod:       procMount_1_0,
 			},
+			{
+				MinimumVersion: api.MajorMinorVersion(1, 35),
+				CheckPod:       procMount_1_35_baseline,
+			},
 		},
 	}
 }
 
+// procMount_1_0 blocks unmasked procMount unconditionally
 func procMount_1_0(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) CheckResult {
-	// TODO: When we remove the UserNamespacesPodSecurityStandards feature gate (and GA this relaxation),
-	// create a new policy version.
-	// Note: pod validation will check for well formed procMount type, so avoid double validation and allow everything
-	// here.
-	if relaxPolicyForUserNamespacePod(podSpec) {
-		return CheckResult{Allowed: true}
-	}
-
 	var badContainers []string
 	forbiddenProcMountTypes := sets.NewString()
 	visitContainers(podSpec, func(container *corev1.Container) {
@@ -99,4 +94,13 @@ func procMount_1_0(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) Chec
 		}
 	}
 	return CheckResult{Allowed: true}
+}
+
+// procMount_1_35_baseline blocks unmasked procMount for pods that are not in a user namespace
+func procMount_1_35_baseline(podMetadata *metav1.ObjectMeta, podSpec *corev1.PodSpec) CheckResult {
+	if relaxPolicyForUserNamespacePod(podSpec) {
+		return CheckResult{Allowed: true}
+	}
+	// If the pod is not in a user namespace, treat it as restricted.
+	return procMount_1_0(podMetadata, podSpec)
 }

staging/src/k8s.io/pod-security-admission/policy/check_procMount_baseline_test.go
@@ -20,21 +20,20 @@ import (
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
 )
 
-func TestProcMount(t *testing.T) {
+func TestProcMountBaseline(t *testing.T) {
 	defaultValue := corev1.DefaultProcMount
 	unmaskedValue := corev1.UnmaskedProcMount
 	otherValue := corev1.ProcMountType("other")
 
-	hostUsers := false
 	tests := []struct {
-		name           string
-		pod            *corev1.Pod
-		expectReason   string
-		expectDetail   string
-		expectAllowed  bool
-		relaxForUserNS bool
+		name          string
+		pod           *corev1.Pod
+		expectReason  string
+		expectDetail  string
+		expectAllowed bool
 	}{
 		{
 			name: "procMount",
@@ -46,14 +45,14 @@ func TestProcMount(t *testing.T) {
 					{Name: "d", SecurityContext: &corev1.SecurityContext{ProcMount: &unmaskedValue}},
 					{Name: "e", SecurityContext: &corev1.SecurityContext{ProcMount: &otherValue}},
 				},
-				HostUsers: &hostUsers,
+				HostUsers: ptr.To(true),
 			}},
 			expectReason:  `procMount`,
 			expectAllowed: false,
 			expectDetail:  `containers "d", "e" must not set securityContext.procMount to "Unmasked", "other"`,
 		},
 		{
-			name: "procMount",
+			name: "procMount with userns",
 			pod: &corev1.Pod{Spec: corev1.PodSpec{
 				Containers: []corev1.Container{
 					{Name: "a", SecurityContext: nil},
@@ -62,24 +61,17 @@ func TestProcMount(t *testing.T) {
 					{Name: "d", SecurityContext: &corev1.SecurityContext{ProcMount: &unmaskedValue}},
 					{Name: "e", SecurityContext: &corev1.SecurityContext{ProcMount: &otherValue}},
 				},
-				HostUsers: &hostUsers,
+				HostUsers: ptr.To(false),
 			}},
-			expectReason:   "",
-			expectDetail:   "",
-			expectAllowed:  true,
-			relaxForUserNS: true,
+			expectReason:  "",
+			expectDetail:  "",
+			expectAllowed: true,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			if tc.relaxForUserNS {
-				RelaxPolicyForUserNamespacePods(true)
-				t.Cleanup(func() {
-					RelaxPolicyForUserNamespacePods(false)
-				})
-			}
-			result := procMount_1_0(&tc.pod.ObjectMeta, &tc.pod.Spec)
+			result := procMount_1_35_baseline(&tc.pod.ObjectMeta, &tc.pod.Spec)
 			if result.Allowed != tc.expectAllowed {
 				t.Fatalf("expected Allowed to be %v was %v", tc.expectAllowed, result.Allowed)
 			}